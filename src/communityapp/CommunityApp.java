ef4fecd428bc14f9a5ca8ea8568c4ce171ab54dd
kabo-m <kabom263@gmail.com>
2025-05-27 16:29:27 +0200

    This was part of my assignments when I was studying Systems Development.

---
+++ src/communityapp/CommunityApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package communityapp;
+
+/**
+ *
+ * @author Kabo
+ */
+public class CommunityApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmMain frmM = new frmMain();
+        frmM.show();
+    }
+    
+}
